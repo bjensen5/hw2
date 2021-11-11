7563efd4cfca62db495ac110df8a9ce40f2395b7
bjensen5 <bjensen5@depaul.edu>
2021-11-10 18:40:13 -0600

    fixed comment

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const jwt = require('jsonwebtoken');
+const Todo = require('../models/Todo')
+const privateKey = process.env.JWT_PRIVATE_KEY;
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   
